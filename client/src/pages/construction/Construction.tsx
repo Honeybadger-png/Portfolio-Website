fdd3ce947d801df98d0a74b3fb47d43c6bd8a79a
Honeybadger-png <9emertgul@gmail.com>
2025-02-12 14:21:44 +0300

    responsiveness

--- client/src/pages/construction/Construction.tsx
+++ client/src/pages/construction/Construction.tsx
@@ -1,16 +1,40 @@
 import "./construction.css";
+import { IconContext } from "react-icons/lib";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+
+
 
 const Construction = () => {
+  const links = {
+    Linkedin: 'https://www.linkedin.com/in/mertkun/',
+    Github: 'https://github.com/Honeybadger-png',
+    Email: 'mertgul1878@gmail.com'
+  }
+
   return (
   <>
     <div className="relative h-screen content-center items-end justify-items-center  text-gray-50">
-      <div className="relative center">
-        <div className="deneme">
-          <h1 >Coming Soon</h1>
-        </div>
+      <div className=" relative center">
+          <div className="deneme">
+            <h1 >Coming Soon</h1>
+          </div>
       </div>
       <div className="socials">
-        <h1>linkedin</h1>
+        <IconContext.Provider value={{className: "icons"}} >
+          <div className="flex flex-row gap-4">
+            <div className="cursor-pointer" onClick={()=>window.open(links.Linkedin,'_blank')}>
+              <AiOutlineLinkedin  />
+            </div>
+            <div className="cursor-pointer" onClick={()=>window.open(links.Github,'_blank')}>
+              <FaGithub />
+            </div>
+            <a href={`mailto:mertgul1878@gmail.com`}>
+              <CiMail />
+            </a>
+          </div>
+        </IconContext.Provider>  
       </div>
     </div>
 
