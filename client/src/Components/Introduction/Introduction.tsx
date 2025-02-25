e6ddab86117205e0e1c2bedf0ae35220fa7491d2
Honeybadger-png <9emertgul@gmail.com>
2025-02-25 17:30:18 +0300

    trying and applying clip-path function of css

--- client/src/Components/Introduction/Introduction.tsx
+++ client/src/Components/Introduction/Introduction.tsx
@@ -1,19 +1,26 @@
 import SkillsCard from "../Card/SkillsCard";
-
+import './Introduction.css';
 
 
 const Introduction = ()=>{
     return(
         <>
-        <div className="w-[50%]">
-            <div className="">
-                <h3>Introduction</h3>
+        <div className="flex ">
+            <div className="introduction">
+                <div className="">
+                    <h3>Introduction</h3>
+                </div>
+                <div className="">
+                    <h1>HEY! I am Mert</h1>
+                </div>
+                <div className="">
+                    <p>I am currently computer engineering student who seeks career in web development. Also, I like designing and making cool websites. I love video games that is why I do 3D models in my free time so I can create my own characters and print them in the future. I like writing so If you like reading... <span className="text-secondary cursor-pointer"><a href="/about">Read More</a></span></p>
+                </div>
             </div>
-            <div className="">
-                <h1>HEY! I am Mert</h1>
-            </div>
-            <div className="">
-                <p>I am currently computer engineering student who seeks career in web development. Also, I like designing and making cool websites. I love video games that is why I do 3D models in my free time so I can create my own characters and print them in the future. I like writing so If you like reading... <span className="text-secondary cursor-pointer"><a href="/about">Read More</a></span></p>
+            <div className="right-introduction">
+                <div className="">
+                    <h1>Introduction</h1>
+                </div>
             </div>
         </div>
         <div className="justify-items-center">
