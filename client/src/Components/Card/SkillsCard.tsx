bb96c8abf09ae1b919cedef2b5c1fe6e1330d2a2
Honeybadger-png <9emertgul@gmail.com>
2025-02-25 22:08:07 +0300

    trying clipping with svg and it worked Nice!

--- client/src/Components/Card/SkillsCard.tsx
+++ client/src/Components/Card/SkillsCard.tsx
@@ -1,10 +1,19 @@
-
+import './SkillsCard.css';
 
 const SkillsCard = () => {
     return (
-        <div className="border-2 border-primary h-[40vh]">
+        <>
+            <div className="denemelik">
+                <svg width="647" height="361" viewBox="0 0 647 361" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <clipPath id='myClip'>
+                        <path d="M184.5 1H1V253L41 293H149H413L480.5 360.5H646.5V109.5L583 46H229.5L184.5 1Z" stroke="black"/>
+                    </clipPath>
+                </defs>
+                </svg>
+            </div>
 
-        </div>
+        </>
     )
 }
 
