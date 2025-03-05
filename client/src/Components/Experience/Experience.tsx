ea341d65c65df0c610bcf95e4c45dc42937c462c
Honeybadger-png <9emertgul@gmail.com>
2025-03-05 23:58:21 +0300

    Playing with framer animations

--- client/src/Components/Experience/Experience.tsx
+++ client/src/Components/Experience/Experience.tsx
@@ -8,7 +8,36 @@ const Experience = ()=>{
         <>
             <motion.div className="border-y-2 border-cyber-orange mt-2" onHoverStart={()=> setIsHover(true)} onHoverEnd={()=> setIsHover(false)}>
                 <motion.h1>
-                    {isHover ? "Hover" : "Not hover"}
+                    {isHover ? 
+                    <motion.div>
+                        <motion.h1 initial={{rotateY:0}} animate={{rotateX:0}} transition={{type:"tween", from:180, ease:"easeIn", duration:0.1}} className="border-2 border-t-0 border-cyber-orange">
+                            Intern
+                        </motion.h1>
+                        <motion.div transition={{ease:"easeInOut"}} className="border-2 border-t-0 border-cyber-orange">
+                            <p>Lorem ipsum dolor sit amet consectetur asdsadasdasdsadad.</p>
+                        </motion.div>
+                    </motion.div>
+                    : 
+                    <motion.div
+                    initial={{
+                        rotateY:0
+                    }}
+                    animate={{
+                        rotateX:360
+                    }}
+                    transition={
+                        {
+                            type:"tween",
+                            from:180,
+                            ease:"easeIn",
+                            duration:.1
+                        }
+                    }
+                    >
+                        <h1>Asumona</h1>
+                        
+                    </motion.div>
+                    }
                 </motion.h1>
             </motion.div>
         </>
