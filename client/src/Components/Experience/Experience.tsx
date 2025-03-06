026c146a974f26645bb92e6d136f9cc557c1efc7
Honeybadger-png <9emertgul@gmail.com>
2025-03-06 15:50:09 +0300

    found a good animation

--- client/src/Components/Experience/Experience.tsx
+++ client/src/Components/Experience/Experience.tsx
@@ -1,45 +1,37 @@
-import {motion} from "framer-motion";
+import {AnimatePresence, easeIn, motion} from "framer-motion";
 import { useState } from "react";
 
 const Experience = ()=>{
     const [isHover, setIsHover] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
 
     return (
         <>
-            <motion.div className="border-y-2 border-cyber-orange mt-2" onHoverStart={()=> setIsHover(true)} onHoverEnd={()=> setIsHover(false)}>
-                <motion.h1>
-                    {isHover ? 
-                    <motion.div>
-                        <motion.h1 initial={{rotateY:0}} animate={{rotateX:0}} transition={{type:"tween", from:180, ease:"easeIn", duration:0.1}} className="border-2 border-t-0 border-cyber-orange">
-                            Intern
-                        </motion.h1>
-                        <motion.div transition={{ease:"easeInOut"}} className="border-2 border-t-0 border-cyber-orange">
-                            <p>Lorem ipsum dolor sit amet consectetur asdsadasdasdsadad.</p>
-                        </motion.div>
-                    </motion.div>
-                    : 
-                    <motion.div
-                    initial={{
-                        rotateY:0
-                    }}
-                    animate={{
-                        rotateX:360
-                    }}
-                    transition={
-                        {
-                            type:"tween",
-                            from:180,
-                            ease:"easeIn",
-                            duration:.1
-                        }
+
+            <motion.h1 initial='initial' whileHover='hovered' className="relative block overflow-hidden whitespace-nowrap text-white">
+                <motion.div variants={{
+                    initial: {y:0},
+                    hovered:{y:"-100%"}
+                }}>
+                    Asumona
+                </motion.div>
+                <motion.div className="cursor-pointer absolute inset-0" 
+                variants={{
+                    initial: {y:"100%"},
+                    hovered:{y:0}
+                }} onClick={()=>setIsClicked(!isClicked)} onHoverEnd={()=>setIsClicked(false)} >
+                    Intern
+                </motion.div>
+                <AnimatePresence>
+                    {
+                        isClicked && (
+                            <motion.div  initial={{opacity:0,height:0}} animate={{opacity:1,height:"auto"}} exit={{opacity:0,height:0}}>
+                                <p>deneme</p>
+                            </motion.div>
+                        )
                     }
-                    >
-                        <h1>Asumona</h1>
-                        
-                    </motion.div>
-                    }
-                </motion.h1>
-            </motion.div>
+                </AnimatePresence>
+            </motion.h1>
         </>
     )
 }
