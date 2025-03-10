943a5efbd0d93b57bb9201633f930c39a977bdbc
Honeybadger-png <9emertgul@gmail.com>
2025-03-10 23:14:24 +0300

    worked about rounded corners and adding borders with clip path

--- client/src/Components/Projects/Projects.tsx
+++ client/src/Components/Projects/Projects.tsx
@@ -1,10 +1,26 @@
+import { styles } from "../../styles"
+import "./Projects.css";
+
 
 
 const Projects = () =>{
     return(
-        <div>
-            <h1>Projects</h1>
-        </div>
+        <>
+            <div className={`${styles.marginComponents} justify-items-center `}>
+                <h1>Projects</h1>
+            </div>
+            <div className="justify-center justify-items-center">
+                <div className="wrap">
+                    <div className="left-button-parent">
+                        <div className="left-button-border"></div>
+                        <div className="left-button-content">
+                            <button className="text-[32px] p-2">WEB</button>
+                        </div>
+                    </div>
+                    <div className="rbtn-category">CSS</div>
+                </div>
+            </div>
+        </>
     )
 }
 
