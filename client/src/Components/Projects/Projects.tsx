25e1fb5fabaf05f488c5180eebb27cdf85a937f7
Honeybadger-png <9emertgul@gmail.com>
2025-03-11 17:59:22 +0300

    started designing projects section

--- client/src/Components/Projects/Projects.tsx
+++ client/src/Components/Projects/Projects.tsx
@@ -9,16 +9,31 @@ const Projects = () =>{
             <div className={`${styles.marginComponents} justify-items-center `}>
                 <h1>Projects</h1>
             </div>
-            <div className="justify-center justify-items-center">
+            <div className={`${styles.marginComponents} justify-center justify-items-center`}>
                 <div className="wrap">
                     <div className="left-button-parent">
-                        <div className="left-button-border"></div>
+                        <div className="left-button-border bg-slate-50"></div>
                         <div className="left-button-content">
-                            <button className="text-[32px] p-2">WEB</button>
+                            <button className="text-[32px] px-6">WEB</button>
                         </div>
                     </div>
-                    <div className="rbtn-category">CSS</div>
+                    <div className="right-button-parent">
+                        <div className="right-button-border"></div>
+                        <div className="right-button-content">
+                            <button className="text-[32px] px-6">CSS</button>
+                        </div>
+                    </div>
                 </div>
+            </div>
+
+            <div className={`${styles.marginComponents} flex relative overflow-hidden h-[50vh]`}>
+                <div className=" flex">
+                    <div className="bg-red-500 w-[40vw]  ">deneme</div>
+                    <div className="bg-slate-400 w-[40vw] ">deneme</div>
+                    <div className="bg-red-500 w-[40vw] ">deneme</div>
+                    <div className="bg-red-500 w-[40vw] ">deneme</div>
+                </div>
+                <button>deneme</button>
             </div>
         </>
     )
