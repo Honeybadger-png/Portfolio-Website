51632f6b742fd4a38b2cdfbdaa3d7f0b8b858085
Honeybadger-png <9emertgul@gmail.com>
2025-04-09 16:54:47 +0300

    working on skills section

--- client/src/Components/Card/SkillsCard.tsx
+++ client/src/Components/Card/SkillsCard.tsx
@@ -1,21 +1,40 @@
 import './SkillsCard.css';
+import {AnimatePresence, motion} from "framer-motion";
+import { useState } from "react";
+
+
+
+
 type propsType = {
     span:string;
     title:string;
-    logo:string;
+    logo?:
+        {
+            title: string;
+            url: string;
+        }[]
+    ;
 }
 
 const SkillsCard = (props:propsType): JSX.Element => {
+    const [isClicked, setIsClicked] = useState(false);
     return (
         <>
             <div className={`h-full border-2 border-cyber-orange ${props.span}`}>
                 <div className='h-full flex flex-col'>
-                    <div className='border-b-2 border-cyber-orange justify-items-center '>
+                    <motion.div className=' justify-items-center cursor-pointer' onClick={()=>setIsClicked(!isClicked)}>
                         <h2>{props.title}</h2>
-                    </div>
-                    <div className=' h-full justify-items-center content-center'>
-                        <h3>{props.logo}</h3>
-                    </div>
+                    </motion.div>
+                    <AnimatePresence>
+                        {
+                            isClicked && (
+                                <motion.div className=' h-full justify-items-center content-center' initial={{opacity:0,height:0}} animate={{opacity:1,height:"auto"}} exit={{opacity:0,height:0}}>
+                                    <h3>{props.logo?.map((value)=>value.title === props.title && value.title )}</h3>
+                                </motion.div>
+                            )
+                        }
+
+                    </AnimatePresence>
                 </div>
             </div>
 
--- client/src/Components/Introduction/Introduction.tsx
+++ client/src/Components/Introduction/Introduction.tsx
@@ -5,15 +5,36 @@ import './Introduction.css';
 
 
 const Introduction = ()=>{
+    enum skillTypes {
+        Web = "Web",
+        Game = "Game",
+        Language = "Language"
+    }
+
+    let logoArray = [
+        {
+            title: skillTypes.Web,
+            url: "Weburl"
+        },
+        {
+            title: skillTypes.Game,
+            url: "Gameurl"
+        },
+        {
+            title: skillTypes.Language,
+            url: "Languageurl"
+        },
+    ]
     return(
         <>
-        <div className="flex ">
+        <div className={`${styles.marginComponents} flex`}>
             <div className="introduction">
                 <div className="">
-                    <h3>Introduction</h3>
+                    <h3 className="text-text-soft">Introduction</h3>
                 </div>
                 <div className="">
-                    <h1>HEY! I am Mert</h1>
+                    <h1 className="text-primary">HEY!</h1>
+                    <h2>I am Mert</h2>
                 </div>
                 <div className="">
                     <p>I am currently computer engineering student who seeks career in web development. Also, I like designing and making cool websites. I love video games that is why I do 3D models in my free time so I can create my own characters and print them in the future. I like writing so If you like reading... <span className="text-secondary cursor-pointer"><a href="/about">Read More</a></span></p>
@@ -26,13 +47,10 @@ const Introduction = ()=>{
         <div className={`justify-items-center ${styles.marginComponents}`}>
             <h1>SKILLS</h1>
         </div>
-        <div className="h-[40vh] grid grid-flow-col grid-rows-2 gap-4">
-            <SkillsCard span="row-span-2" title="HTML&CSS" logo="HTML"/>
-            <SkillsCard span="row-span-1" title="Node" logo="Node"/>
-            <SkillsCard span="row-span-1" title="Blender" logo="Blender"/>
-            <SkillsCard span="row-span-1" title="Figma" logo="Figma"/>
-            <SkillsCard span="row-span-1" title="Next" logo="Next"/>
-            <SkillsCard span="row-span-2" title="React" logo="React"/>
+        <div className="h-full grid grid-cols-3 gap-4">
+            <SkillsCard span="" title="Web"  logo={logoArray} />
+            <SkillsCard span="" title="Game"  logo={logoArray}/>
+            <SkillsCard span="" title="Language"  logo={logoArray} />
         </div>
         </>
     )
